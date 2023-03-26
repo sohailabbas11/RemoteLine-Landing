a38e8754e8b335634f90d10b1bec1b6579368a50
sohailabbas11 <sohailabbas669@gmail.com>
2023-03-26 13:41:27 +0200

    new

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
 import Banner from '../assets/images/Banner.png'
 
 const Navbar = () => {
     return (
-        <div style={{ backgroundImage: `url(${Banner})` }} className='relative flex flex-row bg-sky-900 text-white justify-between h-96 px-5'>
+        <div style={{ backgroundImage: `url(${Banner})` }} className='relative flex flex-row bg-sky-900 text-white h-96 px-5'>
             <div className='fixed flex flex-row gap-8 py-6 h-5 cursor-pointer'>
                 <span>Home</span>
                 <span>About us</span>
