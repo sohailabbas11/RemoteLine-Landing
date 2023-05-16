9d67c27eabb131cd535dafc064a14bc1e8458455
sohailabbas11 <sohailabbas669@gmail.com>
2023-05-16 17:07:38 +0200

    new

--- src/components/Customer.jsx
+++ src/components/Customer.jsx
@@ -4,11 +4,11 @@ const Customer = () => {
     return (
         <div>
             <h1 className='flex justify-center text-5xl font-Alkatra text-green-600 pt-12'>Happy Clients</h1>
-            <div className='flex flex-col justify-center items-center sm:flex-row'>
+            <div className='flex py-8 space-x-4'>
                 <div className='w-1/2'>
                     <img className="" src={require('../images/customer.gif')} alt='' />
                 </div>
-                <div className="w-1/2 flex bg-contain bg-no-repeat bg-right " style={{ backgroundImage: `url(${require('../images/bg2.png')})` }}>
+                <div className="w-1/2 flex bg-cover bg-no-repeat bg-right pt-28 " style={{ backgroundImage: `url(${require('../images/bg2.png')})` }}>
                     <div className='grid grid-rows-5 grid-flow-col gap-10'>
                         <div>Company Logo</div>
                         <div>Company Logo</div>
